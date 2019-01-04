534fe34510bd5be072c37f8eb6a0446ab3ca8b1b
jeremy <jeremy.k.coleman@outlook.com>
2019-01-04 12:47:44 -0500

    updates

--- src/common/router/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ConfigRouter";
-export * from "./exactPath";
-export * from "./path-to-regexp";
-export * from "./PathTemplate";
-export * from "./reactRouter";
-export * from "./Router";
-export * from "./types";
--- src/common/router/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./IPathTemplate";
-export * from "./IPathTemplateOptions";
-export * from "./IPathTestResult";
-export * from "./IRequest";
-export * from "./IRequestHandler";
-export * from "./IRouter";
-export * from "./IRouterManager";
